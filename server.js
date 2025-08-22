ca6118897e05d23c4f91152a34e7f868ccba9cff
Isabela Duetes <isabela.duetes@aluno.senai.br>
2025-08-22 09:22:51 -0300

    feat: cria get by id e get by name

--- server.js
+++ server.js
@@ -9,11 +9,42 @@ app.use(express.json());
 //Rota principal
 app.get("/", (req,res) => {
     res.send("Vamos de Harry Potter");
-})
+});
 
 //Roata dos Bruxos
 app.get("/bruxos", (req, res) => {
     res.json(bruxos);
+});
+
+//Criar a rota do GetByName
+app.get("/bruxos/nome/:nome", (req, res) => {
+    let nome = req.params.nome;
+    nome = nome.toLocaleLowerCase();
+
+    const nomesFiltrados = bruxos.filter(b => b.nome.toLocaleLowerCase().includes(nome));
+
+    if(nomesFiltrados) {
+        res.status(200).json(nomesFiltrados);
+    } else {
+        res.status(404).json({
+            mensagem: "Bruxo(a) não encontrado!"
+        })
+    }
+})
+
+//Criar a rota do GetById
+app.get("/bruxos/:id", (req, res) => {
+    let id = req.params.id;
+    id = parseInt(id)
+    const bruxo = bruxos.find(b => b.id === id);
+
+    if(bruxo) {
+        res.status(200).json(bruxo);
+    } else {
+        res.status(404).json({
+            mensagem: "Bruxo não encontrado!"
+        })
+    }
 });
 
 //Iniciar Servidor
